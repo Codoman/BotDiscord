ed101dd1d2ff1539201bc2e7788827d85c102991
h_apax <dariuscodoman@gmail.com>
2018-05-15 22:12:35 +0200

    add Google Class

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+const Google = require('./commands/google')
+
+
+bot.on('ready', () => {
+    bot.user.setAvatar('./avatar.png')
+})
+
+bot.on('guildMemberAdd', () => {
+    member.createDM().then((channel) => {
+        return channel.send('Welcome ' + member.displayName)
+    }).catch(console.error)
+})
 
 bot.on('message', (message) => {
+    if (Google.match(message))
+        return Google.action(message) 
+        
     if(message.content === 'ping')
         message.reply('pong')
 })
